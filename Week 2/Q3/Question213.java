c932dd68cec90af969bb83b78322a342033feae4
Apurva Lawate <56552569+ApurvaLawate@users.noreply.github.com>
2020-01-26 19:02:21 +0530

    Create Question213.java

---
+++ Week 2/Q3/Question213.java
@@ -0,0 +1,23 @@
+// This is the main class Question
+public class Question213{ 
+    public static void main(String[] args) { 
+		// Object of the main class is created
+		Question213 q = new Question213();
+		// Print method on object of Question class is called
+		q.studentMethod();
+    }
+	
+	// 'print()' method is defined in class Question
+	void print(Question213 object){
+		System.out.print("Well Done!");
+	}
+
+// Define a method named 'studentMethod()' in class Question
+
+// Call the method named 'print()' in class Question
+void studentMethod()
+{
+ Question213 q=new Question213();
+  q.print(q);
+}
+}
